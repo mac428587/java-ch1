cc780a875e751151a58ad89b24bb0bb85f56842e
matthew <mac428508@gmail.com>
2021-02-13 08:30:29 -0600

    wrote Milestone on code to have user input to number into the program and have it tell the lager number in decimal form

--- src/App.java
+++ src/App.java
@@ -1,12 +1,45 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       // Milestone 1
+       // Milestone 1 Java Program to Find largest of Two Decimal Numbers.
+import java.util.Scanner;
+
+public class LargestofTwo {
+	private static Scanner sc;
+	public static void main(String[] args) 
+	{
+		Double number1, number2;
+		sc = new Scanner(System.in);
+		
+		System.out.print(" Please Enter the First Number : ");
+		number1 = sc.nextDouble();	
+		
+		System.out.print(" Please Enter the Second Number : ");
+		number2 = sc.nextDouble();	
+		
+		if(number1 > number2) 
+	    {
+			System.out.println("\n The Largest Number = " + number1);          
+	    } 
+	    else if (number2 > number1)
+	    { 
+	    	System.out.println("\n The Largest Number = " + number2);        
+	    } 
+	    else 
+	    {
+	    	System.out.println("\n Both are Equal");
+	    }		
+	}	
+}
+
 
        // Milestone 2
 
 
+
+
+
        // Milestone 3
 
-       
+
     }
 }
