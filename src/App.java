18c155916d5734efe5f1508cf706fe6f40cb0780
Matt <mac428508@gmail.com>
2021-02-16 16:48:30 -0600

    homework done

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
        // Milestone 1 Java Program to Find largest of Two Decimal Numbers.
-import java.util.Scanner;
+import java.util.Scanner;{
 
 public class LargestofTwo {
 	private static Scanner sc;
@@ -18,15 +18,15 @@ public class LargestofTwo {
 		
 		if(number1 > number2) 
 	    {
-			System.out.println("\n The Largest Number = " + number1);          
+			System.out.println(" The Largest Number = " + number1);          
 	    } 
 	    else if (number2 > number1)
 	    { 
-	    	System.out.println("\n The Largest Number = " + number2);        
+	    	System.out.println(" The Largest Number = " + number2);        
 	    } 
 	    else 
 	    {
-	    	System.out.println("\n Both are Equal");
+	    	System.out.println(" Both are Equal");
 	    }		
 	}	
 }
@@ -34,7 +34,7 @@ public class LargestofTwo {
 
        // Milestone 2 Have user input two numbers have those number print out the average in a decimal.
 
-import java.util.Scanner;
+
 
 public class averageoftwonubers{
 	private static Scanner sc;
@@ -57,8 +57,28 @@ public class averageoftwonubers{
 
 
 
-       // Milestone 3
+       // Milestone 3.  Input the radius of a circle and print the circumference and area. 
+       //The answer should be a decimal value. The formula for circumference is 2 * pi * r 
+       //and the formula for area is pi * r * r. You can use 3.14 for the value of pi in this activity.
+
+       
+{
+   static Scanner sc = new Scanner(System.in);
+   public static void main(String args[])
+   {
+      System.out.print("Enter the radius: ");
+      
+      double radius = sc.nextDouble();
+      
+      double area = Math.PI * (radius * radius);
+      System.out.println("The area of circle is: " + area);
+      
+      double circumference= Math.PI * 2*radius;
+      System.out.println( "The circumference of the circle is:"+circumference) ;
+   }
+}
 
 
     }
 }
+            
